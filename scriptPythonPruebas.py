21d5ff83bcafcab8c0dfbe4ce94e3c81a4067744
JM-GV <gonzalezvargasjm@gmail.com>
2026-02-09 10:16:23 +0100

    Update scriptPythonPruebas.py

--- scriptPythonPruebas.py
+++ scriptPythonPruebas.py
@@ -1,14 +1,6 @@
-def saludar():
-    nombre = input("¿Cómo te llamas? ")
+print(f"¡Hola, Root!")
 
-    if not nombre.strip():
-        print("No ingresaste un nombre.")
-        return
 
-    print(f"¡Hola, {nombre}!")
-    print(f"Tu nombre tiene {len(nombre)} caracteres.")
 
-if __name__ == "__main__":
-    saludar()
 
 
